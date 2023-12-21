2677fa6ed8977e01a3e65d18053414f801655c9e
ArifRahman <arifrahman78640@gmail.com>
2023-12-21 17:58:24 +0600

    TheHonestGuy

--- STL/STLstring.cpp
+++ STL/STLstring.cpp
@@ -1,11 +1,19 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
+// [-(~TheHonestGuy~)-]
 using namespace std;
 
+typedef long long ll;
+typedef double dl;
+
+#define endl "\n"
+#define optimize()  ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define fraction()  cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define mem(a,b) memset (a, b, sizeof(a))
+#define sqr(a) ((a) * (a))
+
 int main() {
-    // Declaration of a vector
+    optimize();
+     // Declaration of a vector
     vector<int> v;
 
     // Adding elements to the vector
@@ -143,3 +151,5 @@ int main() {
 
     return 0;
 }
+
+
